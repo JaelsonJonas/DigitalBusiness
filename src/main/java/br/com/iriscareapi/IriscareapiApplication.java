61e31828ba31b9e18ee80f2009c337cf19bce1de
Marcos Bilobram <marcosbilobram@hotmail.com>
2023-11-15 17:25:06 -0300

    Starter project

---
+++ src/main/java/br/com/iriscareapi/IriscareapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.iriscareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IriscareapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IriscareapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iriscareapi/IriscareapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iriscareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IriscareapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
